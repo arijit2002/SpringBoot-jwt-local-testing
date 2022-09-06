984ff3526843393b07c3c00feff85e9dd6ecd63e
Arijit Das <arijit10das2002@gmail.com>
2022-09-06 23:02:12 +0530

    first commit

---
+++ src/main/java/com/example/jwtTesting/JwtTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwtTesting/JwtTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
