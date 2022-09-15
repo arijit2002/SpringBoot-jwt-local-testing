c33a1b57fa3a70c175faae742b5c702daf2ed1a2
Arijit Das <arijit10das2002@gmail.com>
2022-09-15 09:49:46 +0530

    Update RoleController.java

--- src/main/java/com/example/jwtTesting/controller/RoleController.java
+++ src/main/java/com/example/jwtTesting/controller/RoleController.java
@@ -1,15 +1,20 @@
 package com.example.jwtTesting.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.jwtTesting.entity.Role;
+import com.example.jwtTesting.service.RoleService;
 
 @RestController
 public class RoleController {
 	
+	@Autowired
+	private RoleService roleService;
+	
 	@PostMapping({"/createNewRole"})
 	public Role createNewRole(Role role) {
-		
+		return roleService.createNewRole(role);
 	}
 }
