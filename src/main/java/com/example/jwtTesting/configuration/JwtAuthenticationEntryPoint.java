fa89976a6a7a1f6d3ead93cd8809a8e8d520121d
Arijit Das <arijit10das2002@gmail.com>
2022-09-18 19:28:11 +0530

    dependencies

---
+++ src/main/java/com/example/jwtTesting/configuration/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,5 @@
+package com.example.jwtTesting.configuration;
+
+public class JwtAuthenticationEntryPoint {
+
+}
--- src/main/java/com/example/jwtTesting/controller/UserController.java
+++ src/main/java/com/example/jwtTesting/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.jwtTesting.controller;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +25,15 @@ public class UserController {
 	@PostMapping({"/registerNewUser"})
 	public User registerNewUser(@RequestBody User user) {
 		return userService.registerNewUser(user);
+	}
+	
+	@GetMapping({"/forAdmin"})
+	public String forAdmin() {
+		return "Only for admin";
+	}
+	
+	@GetMapping({"/forUser"})
+	public String forUser() {
+		return "Only for users";
 	}
 }
