70d507c8aa710075091e1773d799216dc2c9aea8
Arijit Das <arijit10das2002@gmail.com>
2022-09-18 21:46:28 +0530

    jwt service

--- src/main/java/com/example/jwtTesting/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/example/jwtTesting/configuration/WebSecurityConfiguration.java
@@ -4,13 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +27,9 @@ public class WebSecurityConfiguration {
 	
 	@Autowired
 	private JwtRequestFilter jwtRequestFilter;
+	
+	@Autowired
+	private UserDetailsService jwtService;
 	
 	@Bean
 	public AuthenticationManager authenticationManager(
@@ -44,5 +52,16 @@ public class WebSecurityConfiguration {
 		
 		return httpSecurity.build();
 		
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	    auth.userDetailsService(jwtService).passwordEncoder(passwordEncoder());
+	      
 	}
 }
