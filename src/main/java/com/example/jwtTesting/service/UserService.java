5cfa760e74dcad5f0354466628db34a26e05de09
Arijit Das <arijit10das2002@gmail.com>
2022-09-19 22:08:07 +0530

    dynamically create users

--- src/main/java/com/example/jwtTesting/service/UserService.java
+++ src/main/java/com/example/jwtTesting/service/UserService.java
@@ -25,6 +25,11 @@ public class UserService {
 	private PasswordEncoder passwordEncoder;
 	
 	public User registerNewUser(User user) {
+		Role role = roleDao.findById("User").get();
+		Set<Role> roles = new HashSet<>();
+		roles.add(role);
+		user.setRole(roles);
+		user.setUserPassword(getEncodedPassword(user.getUserPassword()));
 		return userDao.save(user);
 	}
 	
@@ -47,13 +52,13 @@ public class UserService {
 		adminUser.setRole(adminRoles);
 		userDao.save(adminUser);
 		
-		User user = new User();
-		user.setUserName("Das");
-		user.setUserPassword(getEncodedPassword("das@123"));
-		Set<Role> userRoles = new HashSet<>();
-		userRoles.add(userRole);
-		user.setRole(userRoles);
-		userDao.save(user);
+//		User user = new User();
+//		user.setUserName("Das");
+//		user.setUserPassword(getEncodedPassword("das@123"));
+//		Set<Role> userRoles = new HashSet<>();
+//		userRoles.add(userRole);
+//		user.setRole(userRoles);
+//		userDao.save(user);
 	}
 	
 	public String getEncodedPassword(String password) {
