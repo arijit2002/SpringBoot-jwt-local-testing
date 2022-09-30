26a1bf466be83267cb77a8ae29f816d034738965
Arijit Das <arijit10das2002@gmail.com>
2022-09-30 18:30:56 +0530

    ip

--- src/main/java/com/example/jwtTesting/controller/JwtController.java
+++ src/main/java/com/example/jwtTesting/controller/JwtController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.jwtTesting.entity.JwtRequest;
 import com.example.jwtTesting.entity.JwtResponse;
+import com.example.jwtTesting.service.IPService;
 import com.example.jwtTesting.service.JwtService;
 
 @RestController
@@ -20,6 +21,9 @@ public class JwtController {
 	@Autowired
 	private JwtService jwtService;
 	
+	@Autowired
+	private IPService ipService;
+	
 	@PostMapping({"/authenticate"})
 	public JwtResponse createJwtToken(@RequestBody JwtRequest jwtRequest, HttpServletRequest request) throws Exception{
 		System.out.println(request.getRemoteAddr());
@@ -28,40 +32,41 @@ public class JwtController {
 	
 	@GetMapping({"/test"})
 	public void test(HttpServletRequest request) {
-		String ip = request.getHeader("X-Forwarded-For");  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("Proxy-Client-IP");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("WL-Proxy-Client-IP");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("HTTP_X_FORWARDED");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("HTTP_X_CLUSTER_CLIENT_IP");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("HTTP_CLIENT_IP");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("HTTP_FORWARDED_FOR");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("HTTP_FORWARDED");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("HTTP_VIA");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getHeader("REMOTE_ADDR");  
-	    }  
-	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
-	        ip = request.getRemoteAddr();  
-	    }  
+//		String ip = request.getHeader("X-Forwarded-For");  
+//	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
+//	        ip = request.getHeader("Proxy-Client-IP");  
+//	    }  
+//	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
+//	        ip = request.getHeader("WL-Proxy-Client-IP");  
+//	    }  
+//	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
+//	        ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
+//	    }  
+//	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
+//	        ip = request.getHeader("HTTP_X_FORWARDED");  
+//	    }  
+//	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
+//	        ip = request.getHeader("HTTP_X_CLUSTER_CLIENT_IP");  
+//	    }  
+//	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
+//	        ip = request.getHeader("HTTP_CLIENT_IP");  
+//	    }  
+//	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
+//	        ip = request.getHeader("HTTP_FORWARDED_FOR");  
+//	    }  
+//	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
+//	        ip = request.getHeader("HTTP_FORWARDED");  
+//	    }  
+//	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
+//	        ip = request.getHeader("HTTP_VIA");  
+//	    }  
+//	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
+//	        ip = request.getHeader("REMOTE_ADDR");  
+//	    }  
+//	    if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {  
+//	        ip = request.getRemoteAddr();  
+//	    }
+		String ip=ipService.ipTrace(request);
 	    System.out.println(ip);
 	}
 }
