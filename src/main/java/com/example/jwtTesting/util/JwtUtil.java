de60f1aba46d19a9405f83eeb6130c32511fa90e
Arijit Das <arijit10das2002@gmail.com>
2022-10-05 19:26:07 +0530

    Update JwtUtil.java

--- src/main/java/com/example/jwtTesting/util/JwtUtil.java
+++ src/main/java/com/example/jwtTesting/util/JwtUtil.java
@@ -18,33 +18,40 @@ public class JwtUtil {
 	private static final String SECRET_KEY = "very_secret";
 	private static final int TOKEN_VALIDITY = 3600*5;
 	
+	// get only the username from the token 
 	public String getUserNameFromToken(String token) {
 		return getClaimFromToken(token, Claims::getSubject);
 	}
 	
+	// get a specific claim from the token 
 	private <T> T getClaimFromToken(String token, Function<Claims, T> claimResolver) {
 		final Claims claims = getAllClaimsFromToken(token);
 		return claimResolver.apply(claims);
 	}
 	
+	// get all the claims from the token using signed signature key
 	private Claims getAllClaimsFromToken(String token) {
 		return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
 	}
 	
+	// validate token 
 	public boolean validateToken(String token,UserDetails userDetails) {
 		String userName = getUserNameFromToken(token);
 		return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
 	}
 	
+	// check for token expiration
 	private boolean isTokenExpired(String token) {
 		final Date expirationDate = getExpirationDateFromToken(token);
 		return expirationDate.before(new Date());
 	}
 	
+	// get the expiration date of the token
 	private Date getExpirationDateFromToken(String token) {
 		return getClaimFromToken(token,Claims::getExpiration);
 	}
 	
+	// generate a specific token
 	public String generateToken(UserDetails userDetails) {
 		Map<String,Object> claims = new HashMap<>();
 		
