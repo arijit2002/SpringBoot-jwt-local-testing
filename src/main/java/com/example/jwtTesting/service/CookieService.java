b79f7b5407329b4fa51109874641a5be3ff90890
Arijit Das <arijit10das2002@gmail.com>
2022-10-09 18:15:44 +0530

    Create CookieService.java

---
+++ src/main/java/com/example/jwtTesting/service/CookieService.java
@@ -0,0 +1,21 @@
+package com.example.jwtTesting.service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CookieService {
+	
+	public void createCookie(HttpServletRequest request,HttpServletResponse response,String key,String value) 
+			throws ServletException, IOException{
+		
+		Cookie prefer = new Cookie(key,value);
+		response.addCookie(prefer);
+	}
+}
